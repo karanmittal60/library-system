2d85fa1289b1c698be670ac70f43ed1520d7f62d
karan mittal <mittal.karan60@gmail.com>
2020-04-22 03:36:14 +0530

    Updates

---
+++ precache-manifest.ceee4140f19045f3abe9916fcb7733f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52996cc7a308ad2e39757110b87afd8e",
+    "url": "/library-system/index.html"
+  },
+  {
+    "revision": "34e1f4deb7e0f401b845",
+    "url": "/library-system/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "d5ae7fd7c73430f26aa2",
+    "url": "/library-system/static/css/main.d502ef75.chunk.css"
+  },
+  {
+    "revision": "34e1f4deb7e0f401b845",
+    "url": "/library-system/static/js/2.4f2bde28.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/library-system/static/js/2.4f2bde28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ae7fd7c73430f26aa2",
+    "url": "/library-system/static/js/main.7454350e.chunk.js"
+  },
+  {
+    "revision": "3f2ed3dce6eaa49b00fe",
+    "url": "/library-system/static/js/runtime-main.28685222.js"
+  }
+]);
